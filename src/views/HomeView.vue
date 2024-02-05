6ec1834a26f8f47d4f7eab55bf4c0e549890203a
Esti Tweg <estictweg@hotmail.com>
2024-02-05 12:47:40 +1030

    add heart and red bg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <img alt="heart" class=" heart h1" src="@/assets/heart.svg" width="850" height="850" />
+    <img alt="heart" class=" heart h2" src="@/assets/heart.svg" width="850" height="850" />
   </main>
 </template>
