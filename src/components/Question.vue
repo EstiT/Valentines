b92dca8df20b3c7982f111a6b5efd6806b910913
Esti Tweg <estictweg@hotmail.com>
2024-02-05 17:02:12 +1030

    improve layout

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -3,21 +3,30 @@
 
 <template>
   <svg viewBox="0 0 500 500">
-    <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+    <path id="curve" d="M73,300 C173,181 334,188 436,300" />
     <text width="500">
       <textPath xlink:href="#curve">
         will you be my
       </textPath>
     </text>
   </svg>
+  <img alt="heart" class="little-h" src="@/assets/whiteHeart.png" />
   <h1>valentine?</h1>
 </template>
 
 <style scoped>
 textPath {
   text-transform: uppercase;
-  font-size: 3.25rem;
+  font-size: 3.5rem;
   font-weight: bold;
+}
+
+svg {
+  width: 20rem;
+  position: absolute;
+  top: 38%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 path {
@@ -26,5 +35,14 @@ path {
 
 text {
   fill: #ffffff;
+}
+
+h1 {
+  font-size: 5.5rem;
+}
+
+.little-h {
+  margin: auto;
+  display: block;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+import Question from '../components/Question.vue';
+
+const noCopy = ref('No');
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Question />
+    <RouterLink to="/">Yes</RouterLink>
+    <RouterLink to="/about">{{ noCopy }}</RouterLink>
     <img alt="heart" class="heart h1" src="@/assets/heart.svg" width="850" height="850" />
     <img alt="heart" class="heart h2" src="@/assets/heart.svg" width="850" height="850" />
   </main>
